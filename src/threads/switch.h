c12adf321c43d51b7bccd39f72286d79b6b9c0c1
qbacpey <qbacpey@foxmail.com>
2022-08-07 14:41:21 +0000

    修改switch_threads_frame意味着需要修改： 1.SWITCH_CUR 2.SWITCH_NEXT 这主要是因为switch_threads函数的返回地址和实际参数的下边是，switch_threads_frame 因此需要跳过switch_threads_frame所占内存（使用上文提到的宏）才能读取到指定参数

--- src/threads/switch.h
+++ src/threads/switch.h
@@ -1,6 +1,11 @@
 #ifndef THREADS_SWITCH_H
 #define THREADS_SWITCH_H
 
+#ifndef FPU_SIZE
+#define FPU_SIZE 108
+#endif
+
+
 #ifndef __ASSEMBLER__
 /* switch_thread()'s stack frame. */
 struct switch_threads_frame {
@@ -8,6 +13,7 @@ struct switch_threads_frame {
   uint32_t esi;        /*  4: Saved %esi. */
   uint32_t ebp;        /*  8: Saved %ebp. */
   uint32_t ebx;        /* 12: Saved %ebx. */
+  uint8_t fpu_state[FPU_SIZE];
   void (*eip)(void);   /* 16: Return address. */
   struct thread* cur;  /* 20: switch_threads()'s CUR argument. */
   struct thread* next; /* 24: switch_threads()'s NEXT argument. */
@@ -31,7 +37,7 @@ void switch_thunk(void);
 #endif
 
 /* Offsets used by switch.S. */
-#define SWITCH_CUR 20
-#define SWITCH_NEXT 24
+#define SWITCH_CUR 128
+#define SWITCH_NEXT 132
 
 #endif /* threads/switch.h */
