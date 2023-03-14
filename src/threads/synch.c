755d4f57f7aca737dfb93f62b8ef1250012d978d
qbacpey <qbacpey@foxmail.com>
2023-03-14 07:37:11 +0000

    优先级捐献部分逻辑异动，同时还补充了许多注释： 1. 锁的优先级表示的是持有这把锁的线程所应当拥有的最小优先级， 当有其他线程（除了锁的当前所有者）间接或直接需要获取此锁的时候，锁的优先级会被设置； 2. 线程获取锁的时候需要设置锁的优先级，释放锁的时候需要将优先级归零。 这样做其实很大程度上是为了提升一点性能，毕竟锁的优先级只是作为更新标识，实际参与判断的值是线程的优先级； 3. 如果线程想要获取的锁A已经被其他线程持有了，为了确保自己因该锁阻塞时其他想要自己的资源的高优先级线程， 能将自己的高优先级捐献给持有锁A的线程，无论如何线程因锁阻塞的时候都需要将自己的donate_for设置为该锁； 4. 整个优先级捐献的核心是确保捐献链上涉及的所有锁的优先级都为捐献者优先级中的最大值。 因此捐献函数的实现需要稍微修改一下，具体来说： + 如果目标锁的优先级大于当前线程：已有其他线程捐献了更高的优先级给该锁，无需继续上溯； + 如果目标锁的优先级小于当前线程，但是锁持有者的优先级大等于于当前线程： 目标线程因其他资源受到了更高优先级的捐赠，只需要将锁的优先级更新为当前线程的优先级，无需继续上溯； + 如果目标锁的优先级小于当前线程，锁持有者的优先级也小于于当前线程： 更新锁优先级和线程优先级，随后继续上溯； 最后，循环的结束条件修改为`lock != NULL`，donee通过lock->holder获取

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -27,14 +27,14 @@
 */
 
 #include "threads/synch.h"
+#include "threads/interrupt.h"
+#include "threads/malloc.h"
+#include "threads/thread.h"
 #include <stdio.h>
 #include <string.h>
-#include "threads/interrupt.h"
-#include "threads/thread.h"
-#include "threads/malloc.h"
 
-static void donate_pri_acquire(struct thread* self, struct lock* lock);
-static bool lock_before(const struct list_elem* , const struct list_elem* , void* aux);
+static void donate_pri_acquire(struct thread *, struct lock *);
+static bool lock_before(const struct list_elem *, const struct list_elem *, void *aux);
 
 /* Initializes semaphore SEMA to VALUE.  A semaphore is a
    nonnegative integer along with two atomic operators for
@@ -45,7 +45,7 @@ static bool lock_before(const struct list_elem* , const struct list_elem* , void
 
    - up or "V": increment the value (and wake up one waiting
      thread, if any). */
-void sema_init(struct semaphore* sema, unsigned value) {
+void sema_init(struct semaphore *sema, unsigned value) {
   ASSERT(sema != NULL);
 
   sema->value = value;
@@ -59,7 +59,7 @@ void sema_init(struct semaphore* sema, unsigned value) {
    interrupt handler.  This function may be called with
    interrupts disabled, but if it sleeps then the next scheduled
    thread will probably turn interrupts back on. */
-void sema_down(struct semaphore* sema) {
+void sema_down(struct semaphore *sema) {
   ASSERT(sema != NULL);
   ASSERT(!intr_context());
 
@@ -78,7 +78,7 @@ void sema_down(struct semaphore* sema) {
    decremented, false otherwise.
 
    This function may be called from an interrupt handler. */
-bool sema_try_down(struct semaphore* sema) {
+bool sema_try_down(struct semaphore *sema) {
   bool success;
 
   ASSERT(sema != NULL);
@@ -96,10 +96,10 @@ bool sema_try_down(struct semaphore* sema) {
    and wakes up one thread of those waiting for SEMA, if any.
 
    This function may be called from an interrupt handler. */
-void sema_up(struct semaphore* sema) {
+void sema_up(struct semaphore *sema) {
   ASSERT(sema != NULL);
 
-  struct thread* t = NULL;
+  struct thread *t = NULL;
   DISABLE_INTR({
     if (!list_empty(&sema->waiters)) {
       t = list_entry(list_pop_front(&sema->waiters), struct thread, elem);
@@ -113,7 +113,7 @@ void sema_up(struct semaphore* sema) {
     thread_yield();
 }
 
-static void sema_test_helper(void* sema_);
+static void sema_test_helper(void *sema_);
 
 /* Self-test for semaphores that makes control "ping-pong"
    between a pair of threads.  Insert calls to printf() to see
@@ -134,8 +134,8 @@ void sema_self_test(void) {
 }
 
 /* Thread function used by sema_self_test(). */
-static void sema_test_helper(void* sema_) {
-  struct semaphore* sema = sema_;
+static void sema_test_helper(void *sema_) {
+  struct semaphore *sema = sema_;
   int i;
 
   for (i = 0; i < 10; i++) {
@@ -159,7 +159,7 @@ static void sema_test_helper(void* sema_) {
    acquire and release it.  When these restrictions prove
    onerous, it's a good sign that a semaphore should be used,
    instead of a lock. */
-void lock_init(struct lock* lock) {
+void lock_init(struct lock *lock) {
   ASSERT(lock != NULL);
 
   lock->holder = NULL;
@@ -176,30 +176,29 @@ void lock_init(struct lock* lock) {
    interrupt handler.  This function may be called with
    interrupts disabled, but interrupts will be turned back on if
    we need to sleep. */
-void lock_acquire(struct lock* lock) {
+void lock_acquire(struct lock *lock) {
   ASSERT(lock != NULL);
   ASSERT(!intr_context());
   ASSERT(!lock_held_by_current_thread(lock));
 
-  struct thread* t = thread_current();
+  struct thread *t = thread_current();
 
   enum intr_level old_level = intr_disable();
   --lock->state;
+  // 无论自身优先级与锁优先级相比如何，必须执行优先级捐献逻辑
+  t->donated_for = lock;
   if (lock->state < 0 && lock->holder != NULL) {
-    if (t->e_pri > lock->pri){
-      // 无论自身优先级如何，必须先设置自己的捐献对象
-      t->donated_for = lock;
-      donate_pri_acquire(t, lock);
-    }
-    sema_down(&lock->semaphore);
-    // 此时lock->state必然为1
-  } else {
-    lock->semaphore.value = 0;
+    donate_pri_acquire(t, lock);
   }
+
+  // 统一通过`sema`函数操作信号量（封装）
+  sema_down(&lock->semaphore);
 
   // 不可在禁用中断的时候申请内存
   // malloc_type(record);
-  // 成功获取锁 压入优先级恢复记录
+
+  // 线程优先级必然大于等于它现在所持有的所有锁的最大优先级
+  // 此处设置锁优先级为线程优先级只是处于性能上的考量而已
   lock->pri = t->e_pri;
   lock->holder = t;
   t->donated_for = NULL;
@@ -213,12 +212,12 @@ void lock_acquire(struct lock* lock) {
 
    This function will not sleep, so it may be called within an
    interrupt handler. */
-bool lock_try_acquire(struct lock* lock) {
+bool lock_try_acquire(struct lock *lock) {
   bool success;
 
   ASSERT(lock != NULL);
   ASSERT(!lock_held_by_current_thread(lock));
-  struct thread* t = thread_current();
+  struct thread *t = thread_current();
 
   enum intr_level old_level = intr_disable();
   success = sema_try_down(&lock->semaphore);
@@ -237,13 +236,15 @@ bool lock_try_acquire(struct lock* lock) {
    An interrupt handler cannot acquire a lock, so it does not
    make sense to try to release a lock within an interrupt
    handler. */
-void lock_release(struct lock* lock) {
+void lock_release(struct lock *lock) {
   ASSERT(lock != NULL);
   ASSERT(lock_held_by_current_thread(lock));
 
-  struct thread* t = thread_current();
+  struct thread *t = thread_current();
 
   DISABLE_INTR({
+    // 由于锁现在没有任何持有者了，将锁的优先级归零即可
+    // 反正其他高优先级线程获取锁的时候也会将其更新为自己的优先级
     lock->holder = NULL;
     lock->pri = 0;
 
@@ -253,25 +254,24 @@ void lock_release(struct lock* lock) {
     else
       t->e_pri = list_entry(list_begin(&t->lock_queue), struct lock, elem)->pri;
 
-    if (lock->state < 0)
-      sema_up(&lock->semaphore);
-    else
-      lock->semaphore.value = 1;
     lock->state++;
   });
+  // 线程释放锁之后，系统中优先级最高的线程可能不再是当前线程，因此需要执行`sema_up`让出CPU
+  // 注意需要将`sema_up`放在中断环境之外，以免中断环境重复
+  sema_up(&lock->semaphore);
 }
 
 /* Returns true if the current thread holds LOCK, false
    otherwise.  (Note that testing whether some other thread holds
    a lock would be racy.) */
-bool lock_held_by_current_thread(const struct lock* lock) {
+bool lock_held_by_current_thread(const struct lock *lock) {
   ASSERT(lock != NULL);
 
   return lock->holder == thread_current();
 }
 
 /* Initializes a readers-writers lock */
-void rw_lock_init(struct rw_lock* rw_lock) {
+void rw_lock_init(struct rw_lock *rw_lock) {
   lock_init(&rw_lock->lock);
   cond_init(&rw_lock->read);
   cond_init(&rw_lock->write);
@@ -279,7 +279,7 @@ void rw_lock_init(struct rw_lock* rw_lock) {
 }
 
 /* Acquire a writer-centric readers-writers lock */
-void rw_lock_acquire(struct rw_lock* rw_lock, bool reader) {
+void rw_lock_acquire(struct rw_lock *rw_lock, bool reader) {
   // Must hold the guard lock the entire time
   lock_acquire(&rw_lock->lock);
 
@@ -306,7 +306,7 @@ void rw_lock_acquire(struct rw_lock* rw_lock, bool reader) {
 }
 
 /* Release a writer-centric readers-writers lock */
-void rw_lock_release(struct rw_lock* rw_lock, bool reader) {
+void rw_lock_release(struct rw_lock *rw_lock, bool reader) {
   // Must hold the guard lock the entire time
   lock_acquire(&rw_lock->lock);
 
@@ -337,7 +337,7 @@ void rw_lock_release(struct rw_lock* rw_lock, bool reader) {
 /* Initializes condition variable COND.  A condition variable
    allows one piece of code to signal a condition and cooperating
    code to receive the signal and act upon it. */
-void cond_init(struct condition* cond) {
+void cond_init(struct condition *cond) {
   ASSERT(cond != NULL);
 
   list_init(&cond->waiters);
@@ -363,7 +363,7 @@ void cond_init(struct condition* cond) {
    interrupt handler.  This function may be called with
    interrupts disabled, but interrupts will be turned back on if
    we need to sleep. */
-void cond_wait(struct condition* cond, struct lock* lock) {
+void cond_wait(struct condition *cond, struct lock *lock) {
   ASSERT(cond != NULL);
   ASSERT(lock != NULL);
   ASSERT(!intr_context());
@@ -386,12 +386,12 @@ void cond_wait(struct condition* cond, struct lock* lock) {
    An interrupt handler cannot acquire a lock, so it does not
    make sense to try to signal a condition variable within an
    interrupt handler. */
-void cond_signal(struct condition* cond, struct lock* lock UNUSED) {
+void cond_signal(struct condition *cond, struct lock *lock UNUSED) {
   ASSERT(cond != NULL);
   ASSERT(lock != NULL);
   ASSERT(!intr_context());
   ASSERT(lock_held_by_current_thread(lock));
-  struct thread* t = NULL;
+  struct thread *t = NULL;
   if (!list_empty(&cond->waiters)) {
     list_sort(&cond->waiters, &thread_before, &grater_thread_pri);
     DISABLE_INTR({
@@ -410,10 +410,10 @@ void cond_signal(struct condition* cond, struct lock* lock UNUSED) {
    An interrupt handler cannot acquire a lock, so it does not
    make sense to try to signal a condition variable within an
    interrupt handler. */
-void cond_broadcast(struct condition* cond, struct lock* lock) {
+void cond_broadcast(struct condition *cond, struct lock *lock) {
   ASSERT(cond != NULL);
   ASSERT(lock != NULL);
-  struct thread* t = NULL;
+  struct thread *t = NULL;
   bool flag = false;
   while (!list_empty(&cond->waiters)) {
     DISABLE_INTR({
@@ -443,37 +443,53 @@ void cond_broadcast(struct condition* cond, struct lock* lock) {
     如果发生了优先级捐献，需要返回true
 
  */
-static void donate_pri_acquire(struct thread* self, struct lock* lock) {
-  struct thread* donee = lock->holder;
-  struct thread* donor = self;
-  ASSERT(donee != NULL);
-  ASSERT(donor != NULL);
-  struct lock* record = NULL;
-  while (donor->e_pri > donee->e_pri) {
-    // 只有是针对不同资源的优先级捐献才触发捐献历史保存
-    record = donor->donated_for;
-    donee->e_pri = donor->e_pri;
-    record->pri = donor->e_pri;
+static void donate_pri_acquire(struct thread *self, struct lock *_lock) {
+  struct lock *lock = _lock;
+  struct thread *donee = lock->holder;
+  ;
+  struct thread *donor = self;
+  while (lock != NULL) {
+    ASSERT(donee != NULL);
+    ASSERT(donor != NULL);
+    if (lock->pri >= donor->e_pri) {
+      // 已有其他线程捐献了更高的优先级给该锁，无需继续上溯
+      break;
+    }
+    
+    // 更新锁优先级
+    lock->pri = donor->e_pri;
 
     // 重排锁在资源队列中的顺序
-    list_remove(&record->elem);
-    list_insert_ordered(&donee->lock_queue, &record->elem, &lock_before, NULL);
+    list_remove(&lock->elem);
+    list_insert_ordered(&donee->lock_queue, &lock->elem, &lock_before, NULL);
 
-    // 重排线程在Ready Queue/Wait List队列中的顺序
+    if (donor->e_pri <= donee->e_pri) {
+      // 目标线程因其他资源受到了更高优先级的捐赠，只需要将锁的优先级更新为当前线程的优先级，无需继续上溯
+      break;
+    }
+
+    // 更新线程优先级
+    donee->e_pri = donor->e_pri;
+
+    // `donee`线程在可能位于Ready Queue/Wait List队列中，利用其新优先级，重排之
     if (donee->queue != NULL) {
       list_remove(&donee->elem);
       list_insert_ordered(donee->queue, &donee->elem, &thread_before, &grater_thread_pri);
     }
 
-    if(donee->donated_for == NULL)
+    // `donee`可能没有捐献对象
+    if (donee->donated_for == NULL)
       break;
+
+    // 继续上溯捐献链
     donor = donee;
     donee = donee->donated_for->holder;
+    lock = donor->donated_for;
   }
 }
 
-static bool lock_before(const struct list_elem* elem_a, const struct list_elem* elem_b, void* aux) {
-  struct lock* lock_a = list_entry(elem_a, struct lock, elem);
-  struct lock* lock_b = list_entry(elem_b, struct lock, elem);
+static bool lock_before(const struct list_elem *elem_a, const struct list_elem *elem_b, void *aux) {
+  struct lock *lock_a = list_entry(elem_a, struct lock, elem);
+  struct lock *lock_b = list_entry(elem_b, struct lock, elem);
   return lock_a->pri >= lock_b->pri;
 }
